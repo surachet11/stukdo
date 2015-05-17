13471cfe2f9371ebda5703b8d39203a7fe58f4ae
Surachet Songsakaew <surachet11@Surachets-MacBook-Pro.local>
2015-05-17 21:19:18 +0700

    Added change task custom action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
 
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy, :change]
 
   # GET /tasks
   # GET /tasks.json
@@ -63,6 +63,13 @@ class TasksController < ApplicationController
       format.json { head :no_content }
     end
   end
+
+  def change
+    @task.update_attributes(state: params[:state])
+    respond_to do |format|
+      format.html{redirect_to tasks_path,notice: "Task Uodate"}
+    end  
+  end  
 
   private
     # Use callbacks to share common setup or constraints between actions.
